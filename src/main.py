20b043b7f8a898ce8700314093463137ec409619
Brenno Lemos <dev@brenno.codes>
2024-05-25 00:12:09 -0300

    feat: use db on app

--- src/main.py
+++ src/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+import psycopg2 as pg
+import os
 
 api = FastAPI()
+conn = pg.connect(
+    host=os.getenv("DATABASE_HOST"),
+    port=os.getenv("DATABASE_PORT") or "5432",
+    user="postgres",
+    password="postgres"
+)
 
 @api.get("/")
 def index():
-    return "Hello, world"
+    cursor = conn.cursor()
+    cursor.execute("SELECT gen_random_uuid();")
+    res, *_ = cursor.fetchone()
+    return f"from db: {res}"
